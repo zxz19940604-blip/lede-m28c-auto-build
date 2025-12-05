55b46a3e20784dc322a8fdf9203de3fcb874ab3f
zxz19940604-blip <zxz19940604@gmail.com>
2025-12-05 23:12:42 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -22,12 +22,12 @@ echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf
 echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf
 echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf
 #echo "src-git fancontrol https://github.com/LeeHe-gif/luci-app-fancontrol.git" >> "feeds.conf
-rm -rf files
-cp -r ../files .
-if [ -d "package/zz/luci-theme-alpha" ]; then
-    cd package/zz/luci-theme-alpha
-    git pull || { echo "luci-theme-alpha git pull failed"; exit 1; }
-    cd ../../..
-else
-    git clone https://github.com/derisamedia/luci-theme-alpha.git package/zz/luci-theme-alpha || { echo "luci-theme-alpha git clone failed"; exit 1; }
-fi
+#rm -rf files
+#cp -r ../files .
+#if [ -d "package/zz/luci-theme-alpha" ]; then
+    #cd package/zz/luci-theme-alpha
+    #git pull || { echo "luci-theme-alpha git pull failed"; exit 1; }
+    #cd ../../..
+#else
+    #git clone https://github.com/derisamedia/luci-theme-alpha.git package/zz/luci-theme-alpha || { echo "luci-theme-alpha git clone failed"; exit 1; }
+#fi
