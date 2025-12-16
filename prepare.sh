bf943e44dd5340e0b28764eb1b69f6f7a7feec28
zxz19940604-blip <zxz19940604@gmail.com>
2025-12-16 23:23:11 +0800

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -18,6 +18,7 @@ fi
 cat feeds.conf.default > feeds.conf
 echo "" >> feeds.conf
 echo "src-git qmodem https://github.com/FUjr/QModem.git;main" >> feeds.conf
+echo "src-git fancontrol https://github.com/rockjake/luci-app-fancontrol.git" >> feeds.conf
 rm -rf files
 cp -r ../files .
 if [ -d "package/zz/luci-theme-alpha" ]; then
